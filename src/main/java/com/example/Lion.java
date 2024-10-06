3eed039ab7cad126e2301d7a7e97ddd43692e00d
Kira-Novitskaia <kira.novitskaya@gmail.com>
2024-10-06 13:13:31 +0200

    stepOne

--- src/main/java/com/example/Lion.java
+++ src/main/java/com/example/Lion.java
@@ -4,20 +4,20 @@ import java.util.List;
 
 public class Lion {
 
-    boolean hasMane;
+    private final boolean hasMane;
+    private final Feline feline; // изменения Feline feline
 
-    public Lion(String sex) throws Exception {
+    public Lion(String sex, Feline feline) throws Exception { //изменения Feline feline
+        this.feline = feline; // изменения
         if ("Самец".equals(sex)) {
             hasMane = true;
         } else if ("Самка".equals(sex)) {
             hasMane = false;
         } else {
-            throw new Exception("Используйте допустимые значения пола животного - самей или самка");
+            throw new Exception("Используйте допустимые значения пола животного - самец или самка");
         }
     }
-
-    Feline feline = new Feline();
-
+    //    Feline feline = new Feline();
     public int getKittens() {
         return feline.getKittens();
     }
